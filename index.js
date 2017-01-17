32194904acb39f04329ab5fb28191370d8179c6c
idlviv <idlviv@rambler.ru>
2017-01-17 20:32:18 +0200

    Almost done

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var state = {
+  size : [],
+  winMes : [],
+  items : []
+};
+var d = localStorage.getItem('game');
+if (d){
+  state = JSON.parse(localStorage.getItem('game'));
+}
+
 window.addEventListener('load', function() {
 
   //Ваш код будет здесь
@@ -16,6 +26,7 @@ window.addEventListener('load', function() {
   var i;
   var j;
   var nextMove;
+  var cells;
   
 
   function isNumber(n) {
@@ -29,9 +40,16 @@ window.addEventListener('load', function() {
       }
     startGame.style.display = 'inline-block';
     mainGame.style.display = 'none';
+    localStorage.clear('game');
+    state.winMes.length = 0;
+    state.size.length = 0;
+    state.items.length = 0;
   }
 
+
+
   function generateField(x) {
+    winnerMessage.innerHTML = '';
     field = document.querySelector('.field');
     startGame.style.display = 'none';
     mainGame.style.display = 'inline';
@@ -46,7 +64,24 @@ window.addEventListener('load', function() {
       }
     }
     nextMove = 'x';
-    winnerMessage.innerHTML = '';
+    cells = document.querySelectorAll('.cell');
+    if (state.winMes[0]) {
+      console.log(state.winMes[0]);
+      winnerMessage.innerHTML = state.winMes[0];
+    }
+    for (i=0; i < state.items.length; i++){
+
+      if (nextMove === 'x') {
+        cells[state.items[i]].classList.add('x');
+        nextMove = 'o';
+      } else {
+        cells[state.items[i]].classList.add('o');
+        nextMove = 'x';
+      }
+    }
+
+
+
     btnStartNewGame.addEventListener('click', function startNewGameListener() {
       btnStartNewGame.removeEventListener('click', startNewGameListener);
       clearData();
@@ -54,35 +89,58 @@ window.addEventListener('load', function() {
     if (!isStarted){
     field.addEventListener('click', clickOnFieldListener);
     }
-    isStarted = true;
 
 
+
+    state.size.push(+x);
     function clickOnFieldListener (event) {
-    var clickedCell = event.target.classList;
+      isStarted = true;
+      var clickedCell = event.target;
 
-      if (clickedCell.contains('x') || clickedCell.contains('o')){
+
+      if (clickedCell.classList.contains('x') || clickedCell.classList.contains('o')){
+        return;
+      }
+      if (clickedCell.classList === field || clickedCell.classList === row){
         return;
       }
 
       if (nextMove === 'x') {
-        clickedCell.add('x');
+        clickedCell.classList.add('x');
         nextMove = 'o';
       } else {
-        clickedCell.add('o');
+        clickedCell.classList.add('o');
         nextMove = 'x';
       }
+
+
+
+      var index;
+      console.log (cells.length);
+      index = Array.prototype.indexOf.call(cells, clickedCell);
+        state.items.push(index);
+
+
 
       if (getWinner() === 'x'){
         winnerMessage.innerHTML = 'x-wins';
         field.removeEventListener('click', clickOnFieldListener);
         isStarted = false;
+        state.winMes.push(winnerMessage.innerHTML);
       }
 
       if (getWinner() === 'o') {
         winnerMessage.innerHTML = 'o-wins';
         field.removeEventListener('click', clickOnFieldListener);
         isStarted = false;
+        state.winMes.push(winnerMessage.innerHTML);
       }
+
+
+      console.log (state.items);
+      localStorage.setItem('game', JSON.stringify(state));
+      console.log (localStorage.getItem('game'));
+
     }
   }
 
@@ -106,5 +164,7 @@ window.addEventListener('load', function() {
   }
     input.addEventListener('keydown', setBtnFieldSizeListener);
     btnGenerate.addEventListener('click', setFieldSizeListener);
-
+    if (d){
+      generateField(state.size[0]);
+    }
 });
